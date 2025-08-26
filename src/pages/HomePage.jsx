8c0863f6539d540a6b8aabe5e7b72a73bb90be34
Jorge Matos <jisapjisap@gmail.com>
2025-08-26 15:49:45 +0200

    Add LogoCarousel component to HomePage for enhanced branding display

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import HeroSection from "../sections/HeroSection"
+import LogoCarousel from "../sections/LogoCarousel"
 
 
 const HomePage = () => {
   return (
     <div className='bg-black mx-auto'>
       <HeroSection />
+      <LogoCarousel />
     </div>
   )
 }
