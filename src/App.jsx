5174da80c1d438191fc3a82268dd658d5c3ccd7b
Jorge Matos <jisapjisap@gmail.com>
2025-08-26 13:28:57 +0200

    Add HomePage and HeroSection components to the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 
 import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
     <div className="text-zinc-100 bg-black p-4">
       <Navbar />
+      <div>
+        <HomePage />
+      </div>
     </div>
   ) 
 };
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import HeroSection from "../sections/HeroSection"
+
+
+const HomePage = () => {
+  return (
+    <div className='bg-black mx-auto'>
+      <HeroSection />
+    </div>
+  )
+}
+
+export default HomePage
